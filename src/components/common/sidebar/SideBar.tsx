f2d02a8997a399e43ed59cb1105141b2d8bf1f3d
call203 <call203@daum.net>
2025-03-04 15:25:45 +0900

    feat:library setting + Layout

---
+++ src/components/common/sidebar/SideBar.tsx
@@ -0,0 +1,69 @@
+import { Menu, MenuProps } from "antd";
+import { MENUITEMS } from "./sidebarData";
+import styled from "styled-components";
+
+const SideBar = () => {
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+  };
+
+  return (
+    <SidbarContainer>
+      <LogoContainer>
+        <IconStyle src="/images/defaultProfile.svg" alt="default_profile" />
+        <span>Logo</span>
+      </LogoContainer>
+
+      <Menu
+        onClick={onClick}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        items={MENUITEMS}
+        style={{ flex: 1, overflowY: "auto" }}
+      />
+
+      <SettingContainer>
+        <IconStyle src="/images/settings.svg" alt="settings" />
+        설정
+      </SettingContainer>
+    </SidbarContainer>
+  );
+};
+
+const LogoContainer = styled.div`
+  padding: 1rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const SettingContainer = styled.div`
+  position: absolute;
+  bottom: 2.8rem;
+  left: 1.5rem;
+  width: 100%;
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  font-size: 1.3rem;
+  cursor: pointer;
+`;
+
+const SidbarContainer = styled.div`
+  width: 20rem;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  position: "relative";
+  box-shadow: "0px 4px 4px rgba(0, 0, 0, 0.25)";
+`;
+
+const IconStyle = styled.img`
+  width: 1.5rem;
+  height: auto;
+`;
+
+export default SideBar;
---
+++ src/components/common/sidebar/sidebarData.tsx
@@ -0,0 +1,39 @@
+import { MenuProps } from "antd";
+
+type MenuItem = Required<MenuProps>["items"][number];
+export const MENUITEMS: MenuItem[] = [
+  {
+    key: "Dashboard",
+    label: "대시보드",
+    icon: (
+      <img
+        src="/images/pie.svg"
+        style={{ width: "1.5rem", height: "1.5rem" }}
+      />
+    ),
+  },
+  {
+    key: "local_analysis",
+    label: "지역 별 분석",
+    icon: (
+      <img
+        src="/images/folder.svg"
+        style={{ width: "1.5rem", height: "1.5rem" }}
+      />
+    ),
+    children: [
+      { key: "map", label: "지도" },
+      { key: "statists", label: "지역 별 통계" },
+    ],
+  },
+  {
+    key: "update_data",
+    label: "데이터 업데이트",
+    icon: (
+      <img
+        src="/images/folder.svg"
+        style={{ width: "1.5rem", height: "1.5rem" }}
+      />
+    ),
+  },
+];
