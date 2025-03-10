5d6baf0d164c3046d7bd17174247c69d43b01018
call203 <call203@daum.net>
2025-03-11 02:59:20 +0900

    fix:map lodash

--- src/components/medi_map/NaverMap.tsx
+++ src/components/medi_map/NaverMap.tsx
@@ -5,6 +5,8 @@ import mapStore from "../../store/mapStore";
 import { getPatientsFromRegion } from "../../store/indexded_db/RegionDB";
 import useNaverMapData from "../../hooks/useNaverMapData";
 import useMediMapData, { Area } from "../../hooks/useMediMapData";
+import { debounce } from "lodash";
+import { makeMarkerClustering } from "../../utils/marker-cluster.js";
 
 const NaverMap: React.FC<{
   handleDrawerOpen: () => void;
@@ -18,7 +20,7 @@ const NaverMap: React.FC<{
   const polygonsRef = useRef<Map<string, naver.maps.Polygon>>(new Map());
   const markersRef = useRef<Map<string, naver.maps.Marker>>(new Map());
   const patientMarkersRef = useRef<Map<string, naver.maps.Marker>>(new Map());
-
+  const markerClustererRef = useRef<any>(null);
   //**Refs
   const mapElement = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<naver.maps.Map | null>(null);
@@ -54,6 +56,7 @@ const NaverMap: React.FC<{
       zoom: 15,
       zoomControl: true
     });
+
     setMap(newMap);
   }, [map]);
 
@@ -66,13 +69,18 @@ const NaverMap: React.FC<{
   useEffect(() => {
     if (!map || !smallPolygons) return;
 
-    const handleZoomChange = () => {
+    const handleZoomChange = debounce(() => {
       //1. Init Map
       const currentZoom = map.getZoom();
+
+      const { name, polygonLineColor, color_r, color_g, color_b, fontSize } =
+        getRegionName(currentZoom);
       const mapBounds = expandBounds(
         map.getBounds() as naver.maps.LatLngBounds,
-        0.1
+        0.3
       );
+
+      console.log(currentZoom);
 
       //2. Remove all polygons and
       polygonsRef.current.forEach((polygon) => polygon.setMap(null));
@@ -106,8 +114,6 @@ const NaverMap: React.FC<{
 
       const { boundAreas } = getBoundAreas(polygonsToRender, mapBounds);
 
-      const { name, polygonLineColor, color_r, color_g, color_b, fontSize } =
-        getRegionName(currentZoom);
       if (boundAreas.length === 0) return;
 
       boundAreas.forEach(async (area) => {
@@ -115,7 +121,7 @@ const NaverMap: React.FC<{
         //Create new polygons
         if (!area.coords || area.coords.length === 0) return;
 
-        const patients = await getPatientsFromRegion(area.areaName, name);
+        // console.log(patients);
         //이거 gu는
         const latLngs = area.coords.map(
           ([lng, lat]) => new window.naver.maps.LatLng(lng, lat)
@@ -133,8 +139,8 @@ const NaverMap: React.FC<{
         }
         if (polygon) {
           polygonsRef.current.set(area.areaName, polygon);
-          // polygon.setMap(map);
-          if (!polygon.hasListener("click") && Array.isArray(patients)) {
+          polygon.setMap(map);
+          if (!polygon.hasListener("click")) {
             polygon.addListener("click", () => {
               handleDrawerOpen();
               setAreaName(area.areaName);
@@ -148,6 +154,66 @@ const NaverMap: React.FC<{
             });
           }
         }
+
+        let marker = markersRef.current.get(area.areaName);
+        if (!marker && polygon) {
+          const bounds = polygon.getBounds();
+          if (bounds) {
+            const center = bounds.getCenter();
+            marker = new window.naver.maps.Marker({
+              map,
+              position: center,
+              icon: {
+                content: `
+          <div style="display: flex; align-items: center; justify-content: center;">
+            <span style="font-size:${fontSize}; color:#616161; text-align: center; font-weight:bold ">${
+                  name === "dong" ? area.areaName.split(" ")[2] : area.areaName
+                }</span>
+          </div>
+        `
+              }
+            });
+          }
+        }
+
+        if (marker) {
+          markersRef.current.set(area.areaName, marker);
+
+          polygonsRef.current.forEach((polygon, key) => {
+            const polygonBounds = polygon.getBounds();
+            const marker = markersRef.current.get(key); // 중복 호출 제거
+
+            if (!mapBounds.hasBounds(polygonBounds)) {
+              polygon.setMap(null);
+              if (marker) marker.setMap(null);
+              return; // 불필요한 연산 방지
+            }
+
+            polygon.setMap(map);
+            if (marker) {
+              if (mapBounds.hasLatLng(marker.getPosition())) {
+                marker.setMap(map);
+              } else {
+                marker.setMap(null);
+              }
+            }
+          });
+          // if (!marker.hasListener("click")) {
+          //   window.naver.maps.Event.addListener(marker, "click", () => {
+          //     handleDrawerOpen();
+          //     setAreaName(area.areaName);
+          //     setTotalCost(stats[area.areaName].totalCost);
+          //     setTotalPatients(stats[area.areaName].patientCount);
+          //     setFirstVisitPatients(stats[area.areaName].firstTimeCount);
+          //     setRevisitedPatients(stats[area.areaName].revisitCount);
+          //     setRevenueTrend(stats[area.areaName].revenuMap);
+          //     setAgeGroups(stats[area.areaName].ageGroupsMap);
+          //     setDailyRevenue(stats[area.areaName].dailyRevenueMap);
+          //   });
+          // }
+        }
+
+        // const patients = await getPatientsFromRegion(area.areaName, name);
 
         //📌 2. Calculate stats for each area
         stats[area.areaName] = {
@@ -169,144 +235,92 @@ const NaverMap: React.FC<{
           "50대": 0,
           "60대": 0
         };
-        if (Array.isArray(patients) && patients.length > 0) {
-          patients.forEach((patient) => {
-            const {
-              latitude,
-              longitude,
-              totalCost,
-              chartNumber,
-              visitDate,
-              age
-            } = patient;
-            const visitedPatients = new Set();
-            const revenueMap = stats[area.areaName].revenuMap;
-            const dailyRevenueMap = stats[area.areaName].dailyRevenueMap;
-            let firstTimeCount = 0;
-            let revisitCount = 0;
-            if (latitude == null || longitude == null || !polygon) {
-              return;
-            }
-            if (containsLocation(latitude, longitude, polygon)) {
-              //재방문 환자수 & 초진 환자수
-              if (visitedPatients.has(chartNumber)) {
-                revisitCount++;
-              } else {
-                firstTimeCount++;
-                visitedPatients.add(chartNumber);
-              }
-              //매출액 변화 추이
-              if (visitDate && revenueMap[visitDate]) {
-                revenueMap[visitDate] += totalCost;
-              } else {
-                revenueMap[visitDate] = totalCost;
-              }
-              const ageInYears = parseAge(age);
-              // 연령대에 맞는 카운트 증가
-              if (ageInYears >= 0 && ageInYears <= 9) {
-                ageGroups["아동"]++;
-              } else if (ageInYears >= 10 && ageInYears <= 19) {
-                ageGroups["10대"]++;
-              } else if (ageInYears >= 20 && ageInYears <= 29) {
-                ageGroups["20대"]++;
-              } else if (ageInYears >= 30 && ageInYears <= 39) {
-                ageGroups["30대"]++;
-              } else if (ageInYears >= 40 && ageInYears <= 49) {
-                ageGroups["40대"]++;
-              } else if (ageInYears >= 50 && ageInYears <= 59) {
-                ageGroups["50대"]++;
-              } else {
-                ageGroups["60대"]++;
-              }
-              //1인당 평균 매출액
-              if (visitDate) {
-                // visitDate가 없으면 초기화
-                if (!dailyRevenueMap[visitDate]) {
-                  dailyRevenueMap[visitDate] = {
-                    totalCost: 0,
-                    patientCount: 0
-                  };
-                }
-              }
-              if (visitDate && dailyRevenueMap[visitDate].totalCost) {
-                dailyRevenueMap[visitDate].totalCost += totalCost;
-                dailyRevenueMap[visitDate].patientCount += 1;
-              } else {
-                dailyRevenueMap[visitDate].totalCost = totalCost;
-                dailyRevenueMap[visitDate].patientCount = 1;
-              }
-              stats[area.areaName].totalCost += totalCost;
-              stats[area.areaName].patientCount += 1;
-              stats[area.areaName].firstTimeCount += firstTimeCount;
-              stats[area.areaName].revisitCount += revisitCount;
-              stats[area.areaName].revenuMap = revenueMap;
-              stats[area.areaName].ageGroupsMap = ageGroups;
-              stats[area.areaName].dailyRevenueMap = dailyRevenueMap;
-            }
-          });
-        }
+        // if (Array.isArray(patients) && patients.length > 0) {
+        //   patients.forEach((patient) => {
+        //     const {
+        //       latitude,
+        //       longitude,
+        //       totalCost,
+        //       chartNumber,
+        //       visitDate,
+        //       age
+        //     } = patient;
+        //     const visitedPatients = new Set();
+        //     const revenueMap = stats[area.areaName].revenuMap;
+        //     const dailyRevenueMap = stats[area.areaName].dailyRevenueMap;
+        //     let firstTimeCount = 0;
+        //     let revisitCount = 0;
+        //     if (latitude == null || longitude == null || !polygon) {
+        //       return;
+        //     }
+        //     if (containsLocation(latitude, longitude, polygon)) {
+        //       //재방문 환자수 & 초진 환자수
+        //       if (visitedPatients.has(chartNumber)) {
+        //         revisitCount++;
+        //       } else {
+        //         firstTimeCount++;
+        //         visitedPatients.add(chartNumber);
+        //       }
+        //       //매출액 변화 추이
+        //       if (visitDate && revenueMap[visitDate]) {
+        //         revenueMap[visitDate] += totalCost;
+        //       } else {
+        //         revenueMap[visitDate] = totalCost;
+        //       }
+        //       const ageInYears = parseAge(age);
+        //       // 연령대에 맞는 카운트 증가
+        //       if (ageInYears >= 0 && ageInYears <= 9) {
+        //         ageGroups["아동"]++;
+        //       } else if (ageInYears >= 10 && ageInYears <= 19) {
+        //         ageGroups["10대"]++;
+        //       } else if (ageInYears >= 20 && ageInYears <= 29) {
+        //         ageGroups["20대"]++;
+        //       } else if (ageInYears >= 30 && ageInYears <= 39) {
+        //         ageGroups["30대"]++;
+        //       } else if (ageInYears >= 40 && ageInYears <= 49) {
+        //         ageGroups["40대"]++;
+        //       } else if (ageInYears >= 50 && ageInYears <= 59) {
+        //         ageGroups["50대"]++;
+        //       } else {
+        //         ageGroups["60대"]++;
+        //       }
+        //       //1인당 평균 매출액
+        //       if (visitDate) {
+        //         // visitDate가 없으면 초기화
+        //         if (!dailyRevenueMap[visitDate]) {
+        //           dailyRevenueMap[visitDate] = {
+        //             totalCost: 0,
+        //             patientCount: 0
+        //           };
+        //         }
+        //       }
+        //       if (visitDate && dailyRevenueMap[visitDate].totalCost) {
+        //         dailyRevenueMap[visitDate].totalCost += totalCost;
+        //         dailyRevenueMap[visitDate].patientCount += 1;
+        //       } else {
+        //         dailyRevenueMap[visitDate].totalCost = totalCost;
+        //         dailyRevenueMap[visitDate].patientCount = 1;
+        //       }
+        //       stats[area.areaName].totalCost += totalCost;
+        //       stats[area.areaName].patientCount += 1;
+        //       stats[area.areaName].firstTimeCount += firstTimeCount;
+        //       stats[area.areaName].revisitCount += revisitCount;
+        //       stats[area.areaName].revenuMap = revenueMap;
+        //       stats[area.areaName].ageGroupsMap = ageGroups;
+        //       stats[area.areaName].dailyRevenueMap = dailyRevenueMap;
+        //     }
+        //   });
+        // }
 
-        // console.log(stats[area.areaName].totalCost);
         polygon?.setOptions({
           paths: latLngs,
-
           fillColor: `rgba(${color_r}, ${color_g}, ${color_b}, ${getPolyonColorOpacity(
-            stats[area.areaName].totalCost
+            //stats[area.areaName].totalCost
+            100000
           )})`
         });
         //📌 2. Show Markers
         //Create all Markers
-        let marker = markersRef.current.get(area.areaName);
-        if (!marker && polygon) {
-          const bounds = polygon.getBounds();
-          if (bounds) {
-            const center = bounds.getCenter();
-            marker = new window.naver.maps.Marker({
-              map,
-              position: center,
-              icon: {
-                content: `
-          <div style="display: flex; align-items: center; justify-content: center;">
-            <span style="font-size:${fontSize}; color:#616161; text-align: center; font-weight:bold ">${
-                  name === "dong" ? area.areaName.split(" ")[2] : area.areaName
-                }</span>
-          </div>
-        `
-              }
-            });
-          }
-        }
-        if (marker) {
-          markersRef.current.set(area.areaName, marker);
-          polygonsRef.current.forEach((polygon, key) => {
-            const polygonBounds = polygon.getBounds();
-            const marker = markersRef.current.get(key);
-            if (mapBounds.hasBounds(polygonBounds)) {
-              polygon.setMap(map);
-              if (marker && mapBounds.hasLatLng(marker.getPosition())) {
-                marker.setMap(map);
-              } else if (marker) {
-                marker.setMap(null);
-              }
-            } else {
-              polygon.setMap(null);
-              if (marker) marker.setMap(null);
-            }
-          });
-          if (!marker.hasListener("click") && Array.isArray(patients)) {
-            window.naver.maps.Event.addListener(marker, "click", () => {
-              handleDrawerOpen();
-              setAreaName(area.areaName);
-              setTotalCost(stats[area.areaName].totalCost);
-              setTotalPatients(stats[area.areaName].patientCount);
-              setFirstVisitPatients(stats[area.areaName].firstTimeCount);
-              setRevisitedPatients(stats[area.areaName].revisitCount);
-              setRevenueTrend(stats[area.areaName].revenuMap);
-              setAgeGroups(stats[area.areaName].ageGroupsMap);
-              setDailyRevenue(stats[area.areaName].dailyRevenueMap);
-            });
-          }
-        }
 
         //📌 5. Place patient markers 점
         // let patientMarkers = patientMarkersRef.current.get(area.area);
@@ -341,7 +355,7 @@ const NaverMap: React.FC<{
         //   patientMarkersRef.current.delete(area.area);
         // }
       });
-    };
+    }, 300);
 
     window.naver.maps.Event.addListener(map, "zoom_changed", handleZoomChange);
     window.naver.maps.Event.addListener(map, "idle", handleZoomChange);
