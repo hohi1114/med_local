9f0a831a86ca298f694cb9121003be98a6592ee4
call203 <call203@daum.net>
2025-03-25 17:17:33 +0900

    fix:getcurrenlocation

--- src/utils/api/apis.ts
+++ src/utils/api/apis.ts
@@ -7,7 +7,7 @@ import { RangeDate } from "../../hooks/useRangeDurationDatePicker";
 import {
   postActiveLicenseParams,
   RegionPrivateParams,
-  regionAnalysisParams,
+  regionAnalysisParams
 } from "../../types/params";
 import {
   VisitData,
@@ -15,7 +15,7 @@ import {
   DailyIncomeEgis,
   PatientListEgis,
   PatientIncomeEgis,
-  BackendResponse,
+  BackendResponse
 } from "../ExcelParser";
 
 /**로그인 */
@@ -24,7 +24,7 @@ export const postLogin = async (loginData: LoginParams) => {
   await saveTokensToCookie({
     access_token: data.access_token,
     refresh_token: data.refresh_token,
-    expires_in: data.expires_in,
+    expires_in: data.expires_in
   });
 
   return data;
@@ -39,7 +39,7 @@ export const postActiveLicense = async (
 
 export const postVerifyCode = async (hardwareNumber: string) => {
   const data = await apiRequest("post", "auth/verify", {
-    hardwareFingerprint: hardwareNumber,
+    hardwareFingerprint: hardwareNumber
   });
   return data;
 };
@@ -53,7 +53,7 @@ export const getUserInfo = async () => {
 export const getDashboardData = async (rangeDate: RangeDate) => {
   const data = await apiRequest("post", "/fetch/dashboard_date_patient", {
     startDate: rangeDate.startDate.format("YYYY-MM-DD"),
-    endDate: rangeDate.endDate.format("YYYY-MM-DD"),
+    endDate: rangeDate.endDate.format("YYYY-MM-DD")
   });
   return data;
 };
@@ -67,7 +67,7 @@ export const getAllRegions = async () => {
 export const getAllRegionsEtc = async (rangeDate: RangeDate) => {
   const data = await apiRequest("post", "/fetch/all_region_patient_cost", {
     startDate: rangeDate.startDate.format("YYYY-MM-DD"),
-    endDate: rangeDate.endDate.format("YYYY-MM-DD"),
+    endDate: rangeDate.endDate.format("YYYY-MM-DD")
   });
   return data;
 };
@@ -83,6 +83,11 @@ export const getRegionPrivateData = async (
   return data;
 };
 
+export const getHospitalLocation = async () => {
+  const data = await apiRequest("get", "/users/location");
+  return data;
+};
+
 /**지역별 분석 */
 export const getRegionAnalysis = async (
   regionAnalysisParams: regionAnalysisParams
@@ -93,7 +98,7 @@ export const getRegionAnalysis = async (
     `/fetch/dashboard_${region}_date_region`,
     {
       startDate: rangeDate.startDate.format("YYYY-MM-DD"),
-      endDate: rangeDate.endDate.format("YYYY-MM-DD"),
+      endDate: rangeDate.endDate.format("YYYY-MM-DD")
     }
   );
   return data;
@@ -105,11 +110,11 @@ export const postRefreshToken = async () => {
     logout();
   }
   const data = await apiRequest("post", "/auth/refresh", {
-    refresh_token: refreshToken,
+    refresh_token: refreshToken
   });
   await saveTokensToCookie({
     access_token: data.access_token,
-    refresh_token: data.refresh_token,
+    refresh_token: data.refresh_token
   });
 
   return data.access_token;
@@ -174,7 +179,7 @@ export const uploadDataToBackendEgis = async (
   const dataToUpload = {
     dailyIncome: dailyIncomeData,
     patientList: patientListData,
-    patientIncome: patientIncomeData,
+    patientIncome: patientIncomeData
   };
 
   try {
