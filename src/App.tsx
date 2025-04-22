30afc4b4705be65ea2f63c226c558644288a3290
call203 <call203@daum.net>
2025-04-22 18:13:14 +0900

    fix:demo - minor error

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
           style={{
             width: "30%",
             position: "fixed",
-            top: 0,
+            bottom: 0,
             right: 0,
             zIndex: 100
           }}
