842e5f79514d6dadaf14e22d4a56b70d40a68d1b
call203 <call203@daum.net>
2025-04-23 10:48:51 +0900

    changed port

--- src/local/locationProcessing.ts
+++ src/local/locationProcessing.ts
@@ -75,7 +75,7 @@ export async function fetchRegionData(token: string): Promise<RegionResponse> {
 
 export async function getMappingData(token: string): Promise<MappingResponse> {
   try {
-    const baseURL = "http://3.39.10.210:3000/api";
+    const baseURL = "http://3.39.10.210:3001/api";
 
     const response = await axios.post<MappingResponse>(
       `${baseURL}/data/get_mapping`,
--- src/pages/UpdateDataPage.tsx
+++ src/pages/UpdateDataPage.tsx
@@ -44,7 +44,7 @@ export async function uploadDataToBackend(
   processedData: ProcessDataPayload
 ): Promise<ProcessDataResponse> {
   try {
-    const baseURL = "http://3.39.10.210:3000/api";
+    const baseURL = "http://3.39.10.210:3001/api";
 
     // Prepare the request payload
     const payload = {
