09b4afcd3a344015bc815bdb729a07acd73fd8ac
hohi1114 <hohi1114@naver.com>
2025-05-28 17:34:45 +0900

    dentweb 5/28

--- src/pages/UpdateDataPage.tsx
+++ src/pages/UpdateDataPage.tsx
@@ -131,7 +131,7 @@ export async function uploadDataToBackendHanChart(
   processedData: ProcessDataPayload
 ): Promise<ProcessDataResponse> {
   try {
-    const baseURL = "http://localhost:3001/api";
+    const baseURL = "http://3.39.10.210:3001/api";
 
     // Prepare the request payload
     const payload = {
@@ -174,7 +174,7 @@ export async function uploadDataToBackendDentWeb(
   processedData: ProcessDataPayload
 ): Promise<ProcessDataResponse> {
   try {
-    const baseURL = "http://localhost:3001/api";
+    const baseURL = "http://3.39.10.210:3001/api";
 
     // Prepare the request payload
     const payload = {
